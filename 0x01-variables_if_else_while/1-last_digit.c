ed83e6aa7e735b3a6b8fe313e48714afc7f8cc57
NadaBouyahya <bouyahyanada.solicode@gmail.com>
2023-03-17 19:54:38 +0100

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,12 +15,9 @@ int main(void)
 	last = n % 10;
 	if (last > 5)
 		printf("the last digit of %d is %d and is greater than 5\n", n, last);
-	if (last == 0)
+	else if (last == 0)
 		printf("the last digit of %d is %d and is zero\n", n, last);
 	else
-	{
-		printf("the last digit of %d is %d and is less than 6", n, last);
-		printf(" and not zero\n");
-	}
+		printf("the last digit of %d is %d and is less than 6 and not zero\n", n, last);
 	return (0);
 }
