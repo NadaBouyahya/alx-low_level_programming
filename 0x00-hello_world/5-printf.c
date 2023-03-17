7388f65fc1c7c56a42161d39396a5fbe99ad0809
NadaBouyahya <bouyahyanada.solicode@gmail.com>
2023-03-17 02:32:48 +0100

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
