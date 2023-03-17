5ab451e130f63bf0e2147a995284012f8f0f102d
NadaBouyahya <bouyahyanada.solicode@gmail.com>
2023-03-17 02:52:23 +0100

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,19 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main Entry function
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
