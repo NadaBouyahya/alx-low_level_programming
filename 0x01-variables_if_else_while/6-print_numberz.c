18ba59895d6e0aa334c7d91aead5e7104ee4eb22
NadaBouyahya <bouyahyanada.solicode@gmail.com>
2023-03-18 00:02:32 +0100

    commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,8 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-/*
+/**
  * main - Entry function
  * Return: 0
  */
@@ -9,8 +10,10 @@ int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
 	return (0);
 }
