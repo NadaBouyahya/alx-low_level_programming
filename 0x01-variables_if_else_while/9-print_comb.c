bd4d0e5debbc893b2e652978c3d1e95a9f0665ae
NadaBouyahya <bouyahyanada.solicode@gmail.com>
2023-03-18 00:27:37 +0100

    commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,8 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-/*
+/**
  * main - Entry function
  * Return: 0
  */
@@ -9,8 +10,14 @@ int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	for (n = '0'; n <= '9'; n++)
+	{
+		if ( n == '9')
+			continue;
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
 	return (0);
 }
