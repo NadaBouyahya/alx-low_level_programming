51f81c36e54e3efb8482eb2eea6bc5e88a6fc224
NadaBouyahya <bouyahyanada.solicode@gmail.com>
2023-03-23 22:01:19 +0000

    ok

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,23 +12,19 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a > b && b > c)
 	{
 		largest = a;
 	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
-	else if (b > c)
+	else if (b > a && a > c)
 	{
 		largest = b;
 	}
 	else
 	{
-		largest = C;
+		largest = c;
 	}
 
-return (largest);
+	return (largest);
 }
 
